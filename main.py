f52b13a29d082d6cd2151d1c2a84473c1fa114a6
andrey <andrey@lybkov.ru>
2024-04-21 17:32:11 +0500

    first add

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ slice2 = number - slice1
 part1 = number - (int(number / staff) * staff)
 part2 = int(slice2 / int(number / staff))
 
-def adding_with_zero(staff_list, slice1, slice2, part1, part2, number) -> list: 
+def adding_with_zero(staff_list, slice1, slice2, part1, part2) -> list:
     lst = []
     for s in staff_list[:part2]:
         for i in range(int(slice2 / part2)):
@@ -26,6 +26,8 @@ def adding_with_zero(staff_list, slice1, slice2, part1, part2, number) -> list:
 
     return lst
 
+#Функци "adding_without_zero" нуждается
+#в дополнительном тестировании
 def adding_without_zero(staff_list, number) -> list:
     lst = []
     for s in staff_list:
@@ -37,7 +39,7 @@ def adding_without_zero(staff_list, number) -> list:
 if number % staff == 0:
     lst = adding_without_zero(staff_list, number)
 else:
-    lst = adding_with_zero(staff_list, slice1, slice2, part1, part2, number)
+    lst = adding_with_zero(staff_list, slice1, slice2, part1, part2)
 
 with open('staff_lines', 'w') as f:
     for i in lst:
