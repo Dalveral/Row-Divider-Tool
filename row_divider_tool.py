db94a2ca1fa3f85d3589591b47cb60d4ed18fb69
andrey <andrey@lybkov.ru>
2024-11-20 11:42:23 +0500

    edit readme.md and fix less than zero input

--- row_divider_tool.py
+++ row_divider_tool.py
@@ -1,10 +1,13 @@
 from functions.calculation_func import with_zero, without_zero, base_calculation
 from functions.work_with_files_func import get_staff_list, write_staff_lines, check_staff_file
 
+ZERO = 0
+ONE = 1
+
 
 def main() -> None:
     if not check_staff_file():
-        print('Error find file, please try again.')
+        print('Error find file, please create "staff.txt" and try again.')
         return
 
     try:
@@ -14,21 +17,25 @@ def main() -> None:
         print('Error in input, please try again.')
         return
 
-    if number < 3:
-        print('Error in input, please try again.')
-        return
-
     staff_list = get_staff_list()
     count_staff = len(staff_list)
 
-    if number % count_staff == 0:
+    if number <= count_staff:
+        if number < ONE:
+            print('Error in input, please try again.')
+            return
+
+        write_staff_lines(staff_list[:number])
+        return
+
+    if number % count_staff == ZERO:
         result = without_zero(staff_list, number)
     else:
         slice1, slice2, part1, part2 = base_calculation(number, count_staff)
         result = with_zero(staff_list, slice1, slice2, part1, part2)
 
-    if result:
-        write_staff_lines(result)
+    write_staff_lines(result)
+    print('All done.')
 
 
 if __name__ == '__main__':
